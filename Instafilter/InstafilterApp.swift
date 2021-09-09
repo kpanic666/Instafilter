d0d895956a9d3ca30397e96d4bfb5a733ed491b4
Andrei Korikov <kpanic666@gmail.com>
2021-09-09 14:57:02 +0500

    Initial Commit

---
+++ Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Andrei Korikov on 09.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
