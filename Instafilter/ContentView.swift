a61011902de3c619e074bc9b1a75ab788249320d
Andrei Korikov <kpanic666@gmail.com>
2021-09-12 17:43:57 +0500

    - Implemented Main screen layout - Integrate UIImagePicker to SwiftUI Layout. Images now can be selected and drawn on the screen.

--- Instafilter/ContentView.swift
+++ Instafilter/ContentView.swift
@@ -8,14 +8,69 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
+  @State private var image: Image?
+  @State private var inputImage: UIImage?
+  @State private var filterIntensity = 0.5
+  @State private var showingImagePicker = false
+
+  var body: some View {
+    NavigationView {
+      VStack {
+        ZStack {
+          Rectangle()
+            .fill(Color.secondary)
+          
+          if image != nil {
+            image?
+              .resizable()
+              .scaledToFit()
+          } else {
+            Text("Tap to select a picture")
+              .font(.title2)
+              .foregroundColor(Color.white)
+          }
+        }
+        .onTapGesture {
+          showingImagePicker = true
+        }
+        
+        HStack {
+          Text("Intensity")
+          Slider(value: $filterIntensity)
+        }
+        .padding(.vertical)
+        
+        HStack {
+          Button("Change Filter") {
+            //
+          }
+          
+          Spacer()
+          
+          Button("Save") {
+            //
+          }
+        }
+        
+      }
+      .padding([.leading, .bottom, .trailing])
+      .navigationBarTitle("Instafilter")
+      .sheet(isPresented: $showingImagePicker, onDismiss: loadImage, content: {
+        ImagePicker(image: $inputImage)
+      })
     }
+  }
+  
+  func loadImage() {
+    guard let inputImage = inputImage else {
+      return
+    }
+    image = Image(uiImage: inputImage)
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
---
+++ Instafilter/ImagePicker.swift
@@ -0,0 +1,45 @@
+//
+//  ImagePicker.swift
+//  Instafilter
+//
+//  Created by Andrei Korikov on 12.09.2021.
+//
+
+import SwiftUI
+
+struct ImagePicker: UIViewControllerRepresentable {
+  @Environment(\.presentationMode) var presentationMode
+  @Binding var image: UIImage?
+  
+  func makeUIViewController(context: Context) -> UIImagePickerController {
+    let picker = UIImagePickerController()
+    picker.delegate = context.coordinator
+    return picker
+  }
+  
+  func updateUIViewController(_ uiViewController: UIImagePickerController, context: Context) {
+    
+  }
+  
+  typealias UIViewControllerType = UIImagePickerController
+  
+  class Coordinator: NSObject, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    let parent: ImagePicker
+    
+    init(_ parent: ImagePicker) {
+      self.parent = parent
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+      if let uiImage = info[.originalImage] as? UIImage {
+        parent.image = uiImage
+      }
+      
+      parent.presentationMode.wrappedValue.dismiss()
+    }
+  }
+  
+  func makeCoordinator() -> Coordinator {
+    Coordinator(self)
+  }
+}
