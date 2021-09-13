0e63f518116e7f62b4e830609801a772e5655931
Andrei Korikov <kpanic666@gmail.com>
2021-09-13 19:18:06 +0500

    + Add save image to user albums functionality + Add a logic for the button "Choose filter"

--- Instafilter/ContentView.swift
+++ Instafilter/ContentView.swift
@@ -14,7 +14,9 @@ struct ContentView: View {
   @State private var inputImage: UIImage?
   @State private var filterIntensity = 0.5
   @State private var showingImagePicker = false
-  @State private var currentFilter = CIFilter.sepiaTone()
+  @State private var currentFilter: CIFilter = CIFilter.sepiaTone()
+  @State private var showingFilterSheet = false
+  @State private var processedImage: UIImage?
   
   let context = CIContext()
 
@@ -57,13 +59,25 @@ struct ContentView: View {
         
         HStack {
           Button("Change Filter") {
-            //
+            showingFilterSheet = true
           }
           
           Spacer()
           
           Button("Save") {
-            //
+            guard let processedImage = processedImage else {
+              return
+            }
+            
+            let imageSaver = ImageSaver()
+            imageSaver.successHandler = {
+              print("Success!")
+            }
+            imageSaver.errorHandler = {
+              print("Oops: \($0.localizedDescription)")
+            }
+            
+            imageSaver.writeToPhotoAlbum(image: processedImage)
           }
         }
         
@@ -73,6 +87,18 @@ struct ContentView: View {
       .sheet(isPresented: $showingImagePicker, onDismiss: loadImage, content: {
         ImagePicker(image: $inputImage)
       })
+      .actionSheet(isPresented: $showingFilterSheet) {
+        ActionSheet(title: Text("Select a filter"), buttons: [
+          .default(Text("Crystallize")) { self.setFilter(CIFilter.crystallize()) },
+          .default(Text("Edges")) { self.setFilter(CIFilter.edges()) },
+          .default(Text("Gaussian Blur")) { self.setFilter(CIFilter.gaussianBlur()) },
+          .default(Text("Pixellate")) { self.setFilter(CIFilter.pixellate()) },
+          .default(Text("Sepia Tone")) { self.setFilter(CIFilter.sepiaTone()) },
+          .default(Text("Unsharp Mask")) { self.setFilter(CIFilter.unsharpMask()) },
+          .default(Text("Vignette")) { self.setFilter(CIFilter.vignette()) },
+          .cancel()
+        ])
+      }
     }
   }
   
@@ -82,20 +108,30 @@ struct ContentView: View {
     }
     
     let beginImage = CIImage(image: inputImage)
-    currentFilter.inputImage = beginImage
+    currentFilter.setValue(beginImage, forKey: kCIInputImageKey)
     applyProcessing()
   }
   
   func applyProcessing() {
-    currentFilter.intensity = Float(filterIntensity)
+    let inputKeys = currentFilter.inputKeys
+    if inputKeys.contains(kCIInputIntensityKey) { currentFilter.setValue(filterIntensity, forKey: kCIInputIntensityKey) }
+    if inputKeys.contains(kCIInputRadiusKey) { currentFilter.setValue(filterIntensity * 200, forKey: kCIInputRadiusKey) }
+    if inputKeys.contains(kCIInputScaleKey) { currentFilter.setValue(filterIntensity * 10, forKey: kCIInputScaleKey) }
     
     guard let outputImage = currentFilter.outputImage else {
       return
     }
     
     if let cgimg = context.createCGImage(outputImage, from: outputImage.extent) {
-      image = Image(decorative: cgimg, scale: CGFloat(1.0), orientation: .up)
+      let uiImage = UIImage(cgImage: cgimg)
+      image = Image(uiImage: uiImage)
+      processedImage = uiImage
     }
+  }
+  
+  func setFilter(_ filter: CIFilter) {
+    currentFilter = filter
+    loadImage()
   }
 }
 
